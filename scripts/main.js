c3e7f283196d7dc14b8962d5d76d5cf5209982cf
Antonio Fortino <thoniorf@gmail.com>
2017-04-22 19:23:36 +0200

    Fixed infinite scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
 
+document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOMLoaded");
+
+  var main = document.getElementsByTagName('main');
   var anchors = document.getElementsByTagName('a');
+
+
   for(var i =0; i < anchors.length; i++){
     anchors[i].addEventListener("click", function(event){
       event.preventDefault();
       var anchor_target = document.getElementById(this.getAttribute("href").substring(1));
-
-      if(anchor_target !== ""){
-        var target_scrollY = anchor_target.offsetTop;
-console.log(target_scrollY);
-        var tId = setInterval(function () {
-          var step = window.pageYOffset + 10;
-          console.log(step);
-          window.scroll(0,step);
-          if(step >= target_scrollY){
-            clearInterval(tId);
-          }
-        },1);
-      }
-
+      anchorScroll(anchor_target,10,10,main[0]);
     });
   }
 
 });
+
+function anchorScroll(target,step,timestep,parent){
+  if(target !== ""){
+    var target_scrollY = target.offsetTop;
+    var intervalID = setInterval(function () {
+      var position = window.pageYOffset + step;
+      window.scroll(0,position);
+      var documentEnd = window.pageYOffset + window.innerHeight >= parent.clientHeight;
+      if(position >= target_scrollY || documentEnd){
+        clearInterval(intervalID);
+      }
+    },timestep);
+  }
+}
