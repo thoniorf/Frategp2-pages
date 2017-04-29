2a2ba4aac42d6f488f5f7bc0b094e4f4ae4bd481
Antonio Fortino <thoniorf@gmail.com>
2017-04-29 20:03:57 +0200

    Better scroll animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,40 @@
-
+var globalAnimationID;
+var globalAnchorParent;
+var globalTargetScrollY;
+var globalAnimationStep;
+var globalDocumentEnd;
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOMLoaded");
-
   var main = document.getElementsByTagName('main');
+  globalAnchorParent = main[0];
   var anchors = document.getElementsByTagName('a');
-
-
   for(var i =0; i < anchors.length; i++){
     anchors[i].addEventListener("click", function(event){
       event.preventDefault();
       var anchor_target = document.getElementById(this.getAttribute("href").substring(1));
-      anchorScroll(anchor_target,25,5,main[0]);
+      if(anchor_target !== ""){
+        globalDocumentEnd = window.pageYOffset + window.innerHeight >= globalAnchorParent.clientHeight;
+        globalTargetScrollY = anchor_target.offsetTop;
+        globalAnimationStep = (globalTargetScrollY - window.pageYOffset) / 10;
+        globalAnimationID = requestAnimationFrame(anchorScroll);
+      }
     });
   }
 
 });
+window.addEventListener("load",function(){
 
-function anchorScroll(target,step,timestep,parent){
-  if(target !== ""){
-    var target_scrollY = target.offsetTop;
-    var intervalID = setInterval(function () {
-      var position = window.pageYOffset + step;
-      window.scroll(0,position);
-      var documentEnd = window.pageYOffset + window.innerHeight >= parent.clientHeight;
-      if(position >= target_scrollY || documentEnd){
-        clearInterval(intervalID);
+});
+
+function anchorScroll(){
+      var documentEnd;
+      var position = window.pageYOffset + globalAnimationStep;
+      if(position > globalTargetScrollY){
+        position = globalTargetScrollY;
       }
-    },timestep);
-  }
+      window.scroll(0,position);
+      if(position >= globalTargetScrollY || globalDocumentEnd){
+        cancelAnimationFrame(globalAnimationID);
+      } else {
+        globalAnimationID = requestAnimationFrame(anchorScroll);
+      }
 }
